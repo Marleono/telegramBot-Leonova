61abe46a46b1ea33e52916eee26ba2a82140c5a8
Marleono <73365729+Marleono@users.noreply.github.com>
2021-01-10 00:22:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '1460145085:AAEb5kATyEySMrT-4QTkZbE4wy9M50dkHSA'
+keys = {
+    'франк': 'CHF',
+    'фунт': 'GBP',
+    'крона': 'SEK',
+    'евро': 'EUR',
+    'доллар': 'USD'
+}
---
+++ extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+class Conversion:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException('Невозможно перевести одинаковые валюты')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Валюта {quote} отсутствует')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Валюта {base} отсутствует')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?symbols={quote_ticker},{base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
